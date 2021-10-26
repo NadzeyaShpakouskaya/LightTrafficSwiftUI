bd755a24dd0a61df62d392c1e1d38f1e7b5500d8
Nadzeya Shpakouskaya <shpakovna@gmail.com>
2021-10-26 11:06:52 +0400

    Initial Commit

---
+++ LightTrafficSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LightTrafficSwiftUI
+//
+//  Created by Nadzeya Shpakouskaya on 26/10/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LightTrafficSwiftUI/LightTrafficSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LightTrafficSwiftUIApp.swift
+//  LightTrafficSwiftUI
+//
+//  Created by Nadzeya Shpakouskaya on 26/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct LightTrafficSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
